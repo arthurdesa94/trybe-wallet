1e699bb35f66fb63c1d41499d4363676c599a144
Arthur de Sa <arthurdesa94@gmail.com>
2021-02-02 13:11:32 -0300

    adding 1st req

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
-    <div>
+    <div className="main-container">
       <Switch>
         <Route path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
       </Switch>
     </div>
   );
